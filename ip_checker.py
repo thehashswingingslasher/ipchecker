c1c5f1f96b3e414236c6d37f470eaa25bc9f951c
thehashswingingslasher <averrozen@gmail.com>
2024-11-27 00:17:39 +0700

    Update and rename ip-checker.py to ip_checker.py

--- ip_checker.py
+++ ip_checker.py
@@ -4,4 +4,4 @@ hostname = input ('Enter a domain name: ')
 ip_add = socket.gethostbyname(hostname)
 
 print(f'Domain Name: {hostname}')
-print(f'IP Address: {ip_address}')
+print(f'IP Address: {ip_add}')
